8e8b186a434eba1cadddf611a49f6743a58b987a
AndrzejKucik <andrzej.kucik@gmail.com>
2019-09-11 14:16:58 +0100

    regularization, docstring, svae names

--- rnn_network_model.py
+++ rnn_network_model.py
@@ -11,7 +11,6 @@ import os
 import numpy as np
 import pickle
 import tensorflow as tf
-
 from tensorflow.keras.layers import (Add, BatchNormalization, Bidirectional, Concatenate, Dense,
                                      Dropout, GRU, Layer, Input, ReLU, LSTM, Reshape, SimpleRNN)
 from tensorflow.keras.models import Model
@@ -19,12 +18,55 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.regularizers import l1_l2
 
 # -- File info --
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __copyright__ = 'Andrzej Kucik 2019'
 __author__ = 'Andrzej Kucik'
 __maintainer__ = 'Andrzej Kucik'
 __email__ = 'andrzej.kucik@gmail.com'
-__date__ = '2019-08-23'
+__date__ = '2019-09-11'
+
+
+# Split layer
+class Split(Layer):
+    """Layer splitting the tensor input into a specified number of chunks along given axis."""
+
+    def __init__(self, axis=1, num_splits=1, **kwargs):
+        """
+        Parameters
+        ----------
+        axis : int
+            axis along which to split the input tensor,
+        num_splits : int
+            number of fragments to split the input tensor to.
+        """
+
+        self.axis = axis
+        self.num_splits = num_splits
+        super(Split, self).__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        """
+
+        Parameters
+        ----------
+        inputs : tensorflow.python.ops.variables.VariableMetaclass
+            input tensor
+        kwargs
+
+        Returns
+        -------
+        split
+            list of tensor fragments.
+
+        """
+
+        split = tf.split(inputs, self.num_splits, axis=self.axis)
+
+        return split
+
+    def compute_output_signature(self, input_signature):
+        pass
+
 
 # Argument parser
 parser = ArgumentParser(description='Process arguments')
@@ -79,41 +121,45 @@ parser.add_argument('-res',
                     help='Residual connection?',
                     type=str,
                     default='False')
+parser.add_argument('-reg',
+                    '--reg',
+                    required=False,
+                    help='Regularize?',
+                    type=str,
+                    default='False')
 
 
-# Split layer
-class Split(Layer):
-    """Layer splitting the tensor input into a specified number of chunks  along a given axis."""
+def build_rnn_model(input_shape, bidirectional=False, layers_config=None, rec='simple', res=False, reg=False):
+    """
+    Recurrent neural network premise selection model.
 
-    def __init__(self, axis=1, num_splits=1, **kwargs):
-        """Arguments:
-           axis       - integer, axis along which to perform the split,
-           num_splits - number of split chunks."""
+    Parameters
+    ----------
+    input_shape : tuple
+        triple of integers: (2, seq_len, seq_dim), where seq_len is the length of the input sequences and seq_dim is
+        the dimension of each of the element of the sequence,
+    bidirectional : bool
+        specifies whether to use a bidirectional wrapper for the recurrent layers,
+    layers_config : list
+        number of units for the consecutive dense layers,
+    rec : str
+        type of the recurrent layer; either `lstm`, `gru` or `lstm`,
+    res : bool
+        specifies whether to use a residual connection,
+    reg : bool
+        specifies whether to use regularization (dropout + batch normalization + l1_l2 bias and recurrent regularizer).
 
-        self.axis = axis
-        self.num_splits = num_splits
-        super(Split, self).__init__(**kwargs)
-
-    def call(self, inputs):
-        return tf.split(inputs, self.num_splits, axis=self.axis)
-
-
-def build_rnn_model(input_shape, rec='simple', bidirectional=False, layers_config=None, res=False):
-    """Function building a premise selection model.
-       Arguments:
-       input_shape   - triple of integers: (2, seq_len, seq_dim), where seq_len is the length of the input sequences and
-                       seq_dim is the dimension of each of the element of the sequence.
-       rec           - type of the recurrent layer; either `lstm`, `gru` or `lstm`,
-       bidirectional - Boolean, specifying whether to use a bidirectional wrapper for the recurrent layers,
-       layers_config - list of integers, number of units for the consecutive dense layers,
-       res           - Boolean, specifying whether to use a residual connection.
-
-       Returns:
-       model         - Keras model object.
+    Returns
+    -------
+    model
+         Keras model object; recurrent premise selection network.
     """
 
     # Regularizer
-    reg = l1_l2(l1=0.01, l2=0.01)
+    if reg:
+        regularizer = l1_l2(l1=0.01, l2=0.01)
+    else:
+        regularizer = None
 
     # Recurrent layer
     if rec == 'gru':
@@ -139,66 +185,54 @@ def build_rnn_model(input_shape, rec='simple', bidirectional=False, layers_confi
     if bidirectional:
         # -- Axiom branch
         a = Bidirectional(recurrent(128, return_sequences=True,
-                                    recurrent_regularizer=reg, bias_regularizer=reg,
-                                    dropout=0.5, recurrent_dropout=0.5,
-                                    name='axiom_' + rec + '_0'),
-                          name='axiom_bidirectional_' + rec + '_0')(a)
-        a = Bidirectional(recurrent(128,
-                                    recurrent_regularizer=reg, bias_regularizer=reg,
-                                    dropout=0.5, recurrent_dropout=0.5,
-                                    name='axiom_' + rec + '_1'),
-                          name='axiom_bidirectional_' + rec + '_1')(a)
+                                    recurrent_regularizer=regularizer, bias_regularizer=regularizer,
+                                    name='axiom_' + rec + '_0'), name='axiom_bidirectional_' + rec + '_0')(a)
+        a = Bidirectional(recurrent(128, recurrent_regularizer=regularizer, bias_regularizer=regularizer,
+                                    name='axiom_' + rec + '_1'), name='axiom_bidirectional_' + rec + '_1')(a)
 
         # -- Conjecture branch
         c = Bidirectional(recurrent(128, return_sequences=True,
-                                    recurrent_regularizer=reg, bias_regularizer=reg,
-                                    dropout=0.5, recurrent_dropout=0.5,
-                                    name='conjecture_' + rec + '_0'),
-                          name='conjecture_bidirectional_' + rec + '_0')(c)
-        c = Bidirectional(recurrent(128,
-                                    recurrent_regularizer=reg, bias_regularizer=reg,
-                                    dropout=0.5, recurrent_dropout=0.5,
-                                    name='conjecture_' + rec + '_1'),
-                          name='conjecture_bidirectional_' + rec + '_1')(c)
+                                    recurrent_regularizer=regularizer, bias_regularizer=regularizer,
+                                    name='conjecture_' + rec + '_0'), name='conjecture_bidirectional_' + rec + '_0')(c)
+        c = Bidirectional(recurrent(128, recurrent_regularizer=regularizer, bias_regularizer=regularizer,
+                                    name='conjecture_' + rec + '_1'), name='conjecture_bidirectional_' + rec + '_1')(c)
 
     # - Unidirectional RNN
     else:
         # -- Axiom branch
-        a = recurrent(256, return_sequences=True,
-                      recurrent_regularizer=reg, bias_regularizer=reg,
-                      dropout=0.5, recurrent_dropout=0.5,
+        a = recurrent(256, return_sequences=True, recurrent_regularizer=regularizer, bias_regularizer=regularizer,
                       name='axiom_' + rec + '_0')(a)
-        a = recurrent(256,
-                      recurrent_regularizer=reg, bias_regularizer=reg,
-                      dropout=0.5, recurrent_dropout=0.5,
+        a = recurrent(256, recurrent_regularizer=regularizer, bias_regularizer=regularizer,
                       name='axiom_' + rec + '_1')(a)
 
         # -- Conjecture branch
-        c = recurrent(256, return_sequences=True,
-                      recurrent_regularizer=reg, bias_regularizer=reg,
-                      dropout=0.5, recurrent_dropout=0.5,
+        c = recurrent(256, return_sequences=True, recurrent_regularizer=regularizer, bias_regularizer=regularizer,
                       name='conjecture_' + rec + '_0')(c)
-        c = recurrent(256,
-                      recurrent_regularizer=reg, bias_regularizer=reg,
-                      dropout=0.5, recurrent_dropout=0.5,
+        c = recurrent(256, recurrent_regularizer=regularizer, bias_regularizer=regularizer,
                       name='conjecture_' + rec + '_1')(c)
 
     x = Concatenate(name='conjecture_axiom_concatenate')([c, a])
 
     # - Dense layers
     for n in range(len(layers_config)):
-        x = Dense(layers_config[n], bias_regularizer=reg, name='dense_' + str(n))(x)
-        x = BatchNormalization(name='batch_normalization_' + str(n))(x)
-        x = ReLU(name='re_lu_' + str(n))(x)
-        x = Dropout(0.5, name='dropout_' + str(n))(x)
+        x = Dense(layers_config[n], bias_regularizer=regularizer, name='dense_' + str(n))(x)
+        if reg:
+            x = BatchNormalization(name='batch_normalization_' + str(n))(x)
+            x = ReLU(name='re_lu_' + str(n))(x)
+            x = Dropout(0.5, name='dropout_' + str(n))(x)
+        else:
+            x = ReLU(name='re_lu_' + str(n))(x)
 
         # -- Residual layers
         if res:
-            y = Dense(layers_config[n], name='res_dense_' + str(n), bias_regularizer=reg)(x)
+            y = Dense(layers_config[n], bias_regularizer=reg, name='res_dense_' + str(n))(x)
             x = Add(name='add_' + str(n))([x, y])
-            x = BatchNormalization(name='res_batch_normalization_' + str(n))(x)
-            x = ReLU()(name='res_re_lu_' + str(n))(x)
-            x = Dropout(0.5, name='res_dropout_' + str(n))(x)
+            if reg:
+                x = BatchNormalization(name='res_batch_normalization_' + str(n))(x)
+                x = ReLU()(name='res_re_lu_' + str(n))(x)
+                x = Dropout(0.5, name='res_dropout_' + str(n))(x)
+            else:
+                x = ReLU()(name='res_re_lu_' + str(n))(x)
 
     # - Model output; usefulness of an axiom, given the conjecture
     model_output = Dense(1, activation='sigmoid', name='conjecture_usefulness')(x)
@@ -222,12 +256,14 @@ def main():
     layers_config = [] if (args['layers_config'] is None) else [int(unit) for unit in args['layers_config'].split(',')]
     rec = args['recurrent'].lower()
     res = True if (args['res'].lower() in ['t', 'true', '1']) else False
+    reg = True if (args['reg'].lower() in ['t', 'true', '1']) else False
 
     # Data chunks
     chunks = list(range(len([file for file in os.listdir(data_dir) if 'x_train_rnn' in file])))
 
     # Model
-    model = build_rnn_model(input_shape=(2, 64, 256), bidirectional=bi, layers_config=layers_config, rec=rec, res=res)
+    model = build_rnn_model(input_shape=(2, 64, 256), bidirectional=bi, layers_config=layers_config,
+                            rec=rec, res=res, reg=reg)
     model.summary()
     model.compile(loss='binary_crossentropy', optimizer=RMSprop(lr=0.001), metrics=['accuracy'])
 
@@ -277,16 +313,17 @@ def main():
         if val is not None:
             main_history['val_loss'].append(val_loss / total_num_batches)
             main_history['val_accuracy'].append(val_accuracy / total_num_batches)
+        print(main_history)
 
         # Save history
-        with open('histories/rnn_batch_size={}_epochs={}_val={}'.format(batch_size, epochs, val)
-                  + 'bi={}_layers_config={}_rec={}_res={}.pickle'.format(bi, layers_config, rec, res),
+        with open('histories/rnn_batch_size={}_epochs={}_val={}_'.format(batch_size, epochs, val)
+                  + 'bi={}_layers_config={}_rec={}_res={}_reg={}.pickle'.format(bi, layers_config, rec, res, reg),
                   'wb') as dictionary:
             pickle.dump(main_history, dictionary, protocol=pickle.HIGHEST_PROTOCOL)
 
     # Save trained model
-    model.save('models/rnn_epochs={}_batch_size={}_val={}'.format(batch_size, epochs, val)
-               + 'bi={}_layers_config={}_rec={}_res={}.h5'.format(bi, layers_config, rec, res))
+    model.save('models/rnn_epochs={}_batch_size={}_val={}_'.format(batch_size, epochs, val)
+               + 'bi={}_layers_config={}_rec={}_res={}_reg={}.h5'.format(bi, layers_config, rec, res, reg))
 
     # Test model
     x_test = np.load(os.path.join(data_dir, 'x_test_rnn.npy'), mmap_mode='r')
